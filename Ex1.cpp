0cabca57be602f6f117147b7ceadedf9baa2865c
Danja1206 <105879524+Danja1206@users.noreply.github.com>
2022-06-03 19:12:48 +0300

    Add files via upload

---
+++ Ex1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class Pair1
+{
+
+public:
+    Pair1(const T& _fst, const T& _snd) : fst(_fst), snd(_snd) {}
+    ~Pair1() {}
+    T first() const { return fst; };
+    T second() const { return snd; };
+
+private:
+    T fst, snd;
+};
+
+int main()
+{
+    Pair1<int> p1(6, 9);
+    cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
+    const Pair1<double> p2(3.4, 7.8);
+    cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+}
