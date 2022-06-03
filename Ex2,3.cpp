dfe925fc8695b56872735dc7211358cbf91df233
Danja1206 <105879524+Danja1206@users.noreply.github.com>
2022-06-03 19:13:23 +0300

    Add files via upload

---
+++ Ex2,3.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+template<typename T, typename U>
+class Pair
+{
+
+public:
+    Pair(const T& _fst, const U& _snd) : fst(_fst), snd(_snd) {}
+    ~Pair() {}
+    T first() const { return fst; };
+    U second() const { return snd; };
+
+private:
+    T fst;
+    U snd;
+};
+
+template<typename V>
+class StringValuePair : public Pair<string, V>
+{
+public:
+    StringValuePair(const string& _frs, const V& _sc) : Pair<string, V> (_frs, _sc) {}
+};
+
+int main()
+{
+    Pair<int, double> p1(6, 7.8);
+    cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
+
+    const Pair<double, int> p2(3.4, 5);
+    cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+
+    StringValuePair<int> svp("Amazing", 7);
+    std::cout << "Pair: " << svp.first() << ' ' << svp.second() << '\n';
+    
+    return 0;
+}
